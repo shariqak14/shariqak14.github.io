d677289093ef6ead7e4fff67a44d51a66d6ab5ae
Shariq Khan <shariqak14@gmail.com>
2020-11-01 15:18:16 -0500

    updated about me

--- about_me.py
+++ about_me.py
@@ -7,13 +7,17 @@ class AboutMe:
         self.email = "shariq.a.khan@uconn.edu"
         self.interests = [
             "software engineering",
-            "data science",
-            "data engineering",
+            "machine learning",
             "web development",
         ]
 
     def __str__(self):
-        return f"""I love {self.interests[0]}. I am into sdfkhsdif {self.interests[1]} sdfsfh"""
+        return f"""Lorem ipsum dolor sit amet {self.origin} adipisicing elit. Cum doloremque consequuntur
+		Nemo, {self.education} culpa in illo nobis quasi impedit, dolore tempora obcaecati quod exercitationem. 
+		Odit, nostrum doloremque asperiores ducimus ex excepturi {self.major} rerum enim fugiat tiae 
+		sit rem animi deserunt inventore quaerat, unde laborum dolores, similique eat quos ratione. Aut molestiae 
+		minima laborum {self.email} ullam iste aperiam, eius alias sint, {self.interests[0]}, {self.interests[1]},
+		and {self.interests[2]} beatae molestiae ullam atque quisquam  praesentium nisi. Tempore officiis 😇!"""
 
 
 if __name__ == "__main__":
